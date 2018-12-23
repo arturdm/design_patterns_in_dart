f8c0c35489c76ee09fe134e4bf7a20fdb74c55b1
Artur Stępniewski <artur.stepniewski+github@gmail.com>
2018-12-23 19:44:18 +0100

    Add builder pattern

--- README.md
+++ README.md
@@ -4,9 +4,15 @@
 
 ### Abstract Factory
 
+Still have to find a way to print some object it pointed by a reference.
+
 ### Prototype
 
+There is a nice way of creating objects with a named constructor just like with static factory methods in Java.
+
 ### Builder
+
+There is no support for inner classes.
 
 ### Factory Method
 
---
+++ lib/creational/builder.dart
@@ -0,0 +1,37 @@
+class Driver {
+  String firstName;
+  String lastName;
+
+  Driver({String this.firstName, String this.lastName});
+
+  @override
+  String toString() {
+    return "$firstName $lastName";
+  }
+}
+
+class DriverBuilder {
+  String _firstName;
+  String _lastName;
+
+  DriverBuilder firstName(String firstName) {
+    _firstName = firstName;
+    return this;
+  }
+
+  DriverBuilder lastName(String lastName) {
+    _lastName = lastName;
+    return this;
+  }
+
+  Driver build() {
+    return Driver(firstName: _firstName, lastName: _lastName);
+  }
+}
+
+void main() {
+  DriverBuilder driverBuilder =
+      DriverBuilder().firstName("Robert").lastName("Kubica");
+
+  print("${driverBuilder.build()}'s driver entry created");
+}
