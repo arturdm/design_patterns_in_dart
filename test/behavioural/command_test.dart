a96e092697e6f2cbfcac6735f0c8cca8e4f412ee
Artur Stępniewski <artur.stepniewski@gmail.com>
2019-10-02 09:06:46 +0200

    Add command pattern

---
+++ test/behavioural/command_test.dart
@@ -0,0 +1,80 @@
+import 'package:meta/meta.dart';
+import 'package:test_api/test_api.dart';
+
+void main() {
+  test("should process order commands", () {
+    // given
+    final cook = Cook();
+    final cakeCommand = CakeOrderCommand(receiver: cook, cakeName: "cheesecake");
+    final coffeeCommand = CoffeeOrderCommand(receiver: cook, coffeeName: "latte");
+    final waiter = Invoker();
+
+    // when
+    waiter.placeOrder(cakeCommand);
+    waiter.placeOrder(coffeeCommand);
+
+    // then
+    final actualOrder = cook.getOrder();
+    expect(actualOrder, equals("cheesecake, latte"));
+  });
+}
+
+abstract class Command {
+  void execute();
+}
+
+class CakeOrderCommand implements Command {
+  Cook receiver;
+  String cakeName;
+
+  CakeOrderCommand({
+    @required this.receiver,
+    @required this.cakeName,
+  });
+
+  @override
+  void execute() {
+    receiver.prepareCake(cakeName);
+  }
+}
+
+class CoffeeOrderCommand implements Command {
+  Cook receiver;
+  String coffeeName;
+
+  CoffeeOrderCommand({
+    @required this.receiver,
+    @required this.coffeeName,
+  });
+
+  @override
+  void execute() {
+    receiver.prepareCoffee(coffeeName);
+  }
+}
+
+class Cook {
+  var _currentOrder = "";
+
+  void prepareCake(String name) {
+    _appendOrder(name);
+  }
+
+  void prepareCoffee(String name) {
+    _appendOrder(name);
+  }
+
+  void _appendOrder(String name) {
+    _currentOrder = _currentOrder.isEmpty ? name : "$_currentOrder, $name";
+  }
+
+  String getOrder() {
+    return _currentOrder;
+  }
+}
+
+class Invoker {
+  void placeOrder(Command command) {
+    command.execute();
+  }
+}
